a0534780b8145c1a5893b1943a4c6832f5ef5edd
Krishna Kandula <krishna.c.kandula@gmail.com>
2018-04-18 11:18:03 -0500

    Begin work on NetworkLayer

---
+++ src/main/java/com/krishnakandula/network/network/NetworkMsg.java
@@ -0,0 +1,10 @@
+package com.krishnakandula.network.network;
+
+/**
+ * Created by Krishna Chaitanya Kandula on 4/18/2018.
+ */
+public class NetworkMsg {
+    int sourceId;
+    int destinationId;
+
+}
